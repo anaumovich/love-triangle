50a00e90e134b82695c16f2ef265059162c86648
anaumovich <naumovich.anton@gmail.com>
2018-09-23 02:11:15 +0300

    new update

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+        let count=0;
+        for (let i=0;i<preferences.length; i++){
+            let firstIndex = i+1; // первый идекс первое значение
+            let firstValue =  preferences[i];// первое значение любит второй индекс
+            let secondValue = preferences[firstValue-1];// второе значение любит третий индекс
+            let thirdValue = preferences[secondValue-1];
+
+            if (thirdValue===firstIndex && firstIndex!==firstValue){
+                count++;
+            }
+        }
+        let result = count/3;
+        console.log(result);
+        return result;
 };
